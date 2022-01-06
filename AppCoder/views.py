dc40592352cede0649bc5b7c2b143049915f6161
Francisco Piñeiro Rossi <panchopr_91@hotmail.com>
2022-01-06 00:06:02 -0300

    segundo commit

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -10,3 +10,14 @@ def curso(self):
     documentoDeTexto = f"--->Curso: {curso.nombre}   Camada: {curso.camada}"
 
     return HttpResponse(documentoDeTexto)
+
+def inicio(request):
+    return HttpResponse('vista inicio')
+def cursos(request):
+    return HttpResponse('vista cursos')
+def profesores(request):
+    return HttpResponse('vista profesores')
+def estudiantes(request):
+    return HttpResponse('vista estudiantes')
+def entregables(request):
+    return HttpResponse('vista entregables')
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from AppCoder.views import curso
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('curso/', curso)
+    path('AppCoder/', include('AppCoder.urls'))
 ]
